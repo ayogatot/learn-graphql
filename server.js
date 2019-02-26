8991d9fed5dcbe985417cd2992ba93c18ee9b540
Azerino Yogananta Gatot Subroto <azerino25@gmail.com>
2019-02-26 22:16:52 +0700

    implement express module to run a webserver

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var {graphql, buildSchema} = require('graphql');
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var {buildSchema} = require('graphql');
 
 var schema = buildSchema(`
   type Query {
@@ -7,11 +9,16 @@ var schema = buildSchema(`
 `);
 
 var root = {
-    hello: () => {
-        return 'Hello world!';
-    },
+  hello: () => {
+    return 'Hello world!';
+  },
 };
 
-graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response);
-});
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
